5b9de93f2818a4324088d53545a594d45abeb30a
Ben Edridge <BenEdridge@users.noreply.github.com>
2020-08-16 00:21:47 +1000

    Add basic mappings and raw data
    See:
    Peglist, NATO Alpha

---
+++ my_config/pegListMapping.js
@@ -0,0 +1,49 @@
+// Rhyming peg list eg. gun = 1, 2 = shoe
+
+const rhymeList = [
+  'gun',
+  'shoe',
+  'tree',
+  'door',
+  'hive',
+  'sticks',
+  'heaven',
+  'bait',
+  'wine',
+  'pen'
+];
+
+// Visually based
+
+/*
+Some possible number shape images are:
+
+0 = ball, egg, doughnut, ring
+1 = candle, stick, spear, pole, pillar, wire
+2 = swan, clothes hangar, whale
+3 = butterfly, handcuffs, heart, trident, half a bottle, fish, bag, rabbit
+4 = flag, sailboat, chair, satellite dish
+5 = hook, snake, Superman, seahorse, a wave or water
+6 = elephant's trunk, golf club, cherry, combination lock, curled snake, spoon
+7 = boomerang, axe, scythe, diving board, ship
+8 = snowman, hourglass, figure 8
+9 = balloon on a string, net
+*/
+
+const visualList = [
+  'ball',
+  'sword',
+  'swan',
+  'butterfly',
+  'chair',
+  'snake',
+  'golf',
+  'axe',
+  'hourglass',
+  'balloon',
+];
+
+module.exports = {
+  visualList,
+  rhymeList
+}
